3d337a6756c99f00650039c6ecd2244e1822aba3
JCam98 <Campbelljustin989@gmail.com>
2021-02-26 14:10:00 -0600

    Update read_animals.py

--- homework1/read_animals.py
+++ homework1/read_animals.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3      
+
 ''' Property of Justin Campbell; UT eID: jsc4348                                                                                                                                                                                            
     Purpose of Use: COE 332: Software Engineering and Design                                                                                                                                                                                
     Script File Name: read_animals.py                                                                                                                                                                                                       
@@ -9,29 +11,17 @@ Description: This script constitutes the second part of two scripts
 used for homework assignment #1. This program uses the I/O method "open()"                                                                                                                                                                  
 to read in the contents of the JSON file "animals.JSON" to a JSON object.                                                                                                                                                                   
 The random module is then used to randomly choose one of the animal descriptions                                                                                                                                                            
-to output to the console.'''
+to output to the console.'''                                                                                                                                                                                                                
 
-''' Use shabang to specify the python version for running the program. This makes the script compatible with a container platform '''
-
-#!/usr/bin/env python3                                                                                                                                                                                                                      
-
-''' The "sys" module is used to eliminate the need to hardcode the import of the "animals.json" file into the script: '''
+''' The "sys" module is used to eliminate the need to hardcode the import of a json file into the script: '''
 
 import json, random, sys
 
-''' Open the json file "animals.json" in read-only mode and assign contents                                                                                                                                                                 
-of file to datastructure "animals": '''
+''' Read in a "json" file from the terminal '''
 
 with open(sys.argv[1],'r') as f:
         animals= json.load(f)
-
-''' Note: The contents of the "animals.json" file could have been read in to the script through hardcoding using the following code block:                                                                                                  
-                                                                                                                                                                                                                                            
-with open('animals.json', 'r') as f:                                                                                                                                                                                                        
-        animals = json.load(f)                                                                                                                                                                                                              
-                                                                                                                                                                                                                                            
-'''
-
+        
 ''' Print the details of one of the animals at random to the console: '''
 
 print(animals['animals'][random.randint(0,20)])
